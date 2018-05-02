eaf5130d10aa5637ee9a6403fc518e2ad78a6057
Chen Mualem <chenmu10@gmail.com>
2018-05-02 13:58:31 +0300

    added profile page and review empty component

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
-import TopicList from './views/Topics.vue'
-import Login from './views/Login.vue'
-import UserEdit from './views/UserEdit.vue'
-import ReviewEdit from './views/ReviewEdit.vue'
+// import TopicList from './views/Topics.vue'
+// import Login from './views/Login.vue'
+// import UserEdit from './views/UserEdit.vue'
+// import ReviewEdit from './views/ReviewEdit.vue'
 import Profile from './views/Profile.vue'
 
 Vue.use(Router)
@@ -14,10 +14,10 @@ export default new Router({
   routes: [
     { path: '/', name: 'home', component: Home},
     { path: '/about', name: 'about', component: About},
-    { path: '/topicList', name: 'topics', component: TopicList},
-    { path: '/login', name: 'login', component: Login},
-    { path: '/user/edit', name: 'user edit', component: UserEdit},
-    { path: '/review/edit', name: 'review edit', component: ReviewEdit},
+    // { path: '/topicList', name: 'topics', component: TopicList},
+    // { path: '/login', name: 'login', component: Login},
+    // { path: '/user/edit', name: 'user edit', component: UserEdit},
+    // { path: '/review/edit', name: 'review edit', component: ReviewEdit},
     { path: '/profile', name: 'profile', component: Profile}
   ]
 })
