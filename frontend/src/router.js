e384df0e10724439588805c157285c117d0e54ed
bshimrit <bshimrit@gmail.com>
2018-05-02 12:05:59 +0300

    router pages

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,20 +2,22 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import TopicList from './views/Topics.vue'
+import Login from './views/Login.vue'
+import UserEdit from './views/UserEdit.vue'
+import ReviewEdit from './views/ReviewEdit.vue'
+import Profile from './views/Profile.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    }
+    { path: '/', name: 'home', component: Home},
+    { path: '/about', name: 'about', component: About},
+    { path: '/topicList', name: 'topics', component: TopicList},
+    { path: '/login', name: 'login', component: Login},
+    { path: '/user/edit', name: 'user edit', component: UserEdit},
+    { path: '/review/edit', name: 'review edit', component: ReviewEdit},
+    { path: '/profile', name: 'profile', component: Profile}
   ]
 })
