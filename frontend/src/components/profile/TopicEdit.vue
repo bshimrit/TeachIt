1a1e7cd2faca6d1cfaa27bd5db45e73f0c8f5b71
Chen Mualem <chenmu10@gmail.com>
2018-05-06 20:08:33 +0300

    add new class for teacher

--- frontend/src/components/profile/TopicEdit.vue
+++ frontend/src/components/profile/TopicEdit.vue
@@ -4,7 +4,7 @@
                 <div>
                   <div class="row">
                     <div class="input-field col s12">
-                      <input type="text" class="validate">
+                      <input v-model="lessonToUpdate.topicId" type="text" class="validate">
                       <label class="left active">TopicId</label>
                     </div>
                     </div>
@@ -20,27 +20,33 @@
                         <label class="left active">Subtitle</label>
                     </div>
                   </div> -->
+                   <div class="row">
+                    <div class="input-field col s12">
+                      <textarea  v-model="lessonToUpdate.shortDes" class="materialize-textarea"></textarea>
+                        <label class="left active">Summary</label>
+                    </div>
+                  </div>
                   <div class="row">
                     <div class="input-field col s12">
-                      <textarea class="materialize-textarea"></textarea>
+                      <textarea  v-model="lessonToUpdate.longDes" class="materialize-textarea"></textarea>
                         <label class="left active">Description</label>
                     </div>
                   </div>
                    <div class="row">
                     <div class="input-field col s12">
-                       <input type="text" class="validate">
+                       <input type="text"  v-model="lessonToUpdate.level" class="validate">
                         <label class="left active">Level</label>
                     </div>
                   </div>
                   <div class="row">
                     <div class="input-field col s12">
-                      <input type="number" class="validate">
+                      <input type="number" class="validate" v-model="lessonToUpdate.pricePerHour">
                         <label class="left active">Price/Hour</label>
                     </div>
                   </div>
                   <div class="row">
                     <div class="input-field col s12">
-                      <input type="text" class="validate">
+                      <input type="text" class="validate" v-model="lessonToUpdate.topicImage">
                         <label class="left active">Image url</label>
                     </div>
                   </div>
@@ -51,17 +57,19 @@
                     </div>
                   </div>
 
-                  <button class="btn green accent-3">Add Class</button>
+                  <a @click="saveLesson" class="btn green accent-3">Add Class</a>
                 </div> 
 
 </section>
 </template>
 
 <script>
+import teacherTopicService from "@/services/TeacherTopicService.js";
+
 export default {
   data() {
     return {
-      lessonToUpdate: {name:'a'}
+      lessonToUpdate: teacherTopicService.emptyTeacherTopic()
     };
   },
   props: {
@@ -72,21 +80,24 @@ export default {
   watch: {},
   computed: {},
   methods: {
-    saveClass() {
-     console.log('lesson', this.lessonToUpdate);
-      // this.$store
-      //   .dispatch({ type: "saveTeacherTopic", teacherTopic: this.class })
-      //   .then(added => {
-      //       window.alert("Updated");
-      //     console.log("added");
-      //   })
-      //   .catch(err => {
-      //      window.alert("Failed");
-      //     console.log("failed:" + err);
-      //   });
+    saveLesson() {
+       var userId = this.$route.params.userId;
+       this.lessonToUpdate.teacherId= this.$route.params.userId;
+       this.lessonToUpdate.topicId= "5ae97336b8ed24ed05f66112";
+      console.log('lesson', this.lessonToUpdate);
+      this.$store
+        .dispatch({ type: "saveTeacherTopic", teacherTopic: this.lessonToUpdate })
+        .then(added => {
+            window.alert("Updated lesson");
+          console.log("added");
+        })
+        .catch(err => {
+           window.alert("Failed lesson");
+          console.log("failed:" + err);
+        });
     }
   },
-  components: {}
+  components: {teacherTopicService}
 };
 </script>
 
--- frontend/src/services/TeacherTopicService.js
+++ frontend/src/services/TeacherTopicService.js
@@ -1,71 +1,72 @@
-
-const TOPIC_TEACHER_URL = '/teacherTopic'
+const TOPIC_TEACHER_URL = "/teacherTopic";
 
 function emptyTeacherTopic() {
-    return {text:'',price:[], topics:[]}
+  return {
+    topicId: null,
+    teacherId: null,
+    level: "",
+    pricePerHour: null,
+    rating: null,
+    shortDes: "",
+    longDes:"",
+    topicImage: ""
+  };
 }
 
 function getTeacherTopics(filter) {
-    var criteria = '?';
-    if (filter){
-        if (filter.text) criteria += `text=${filter.text}&`;
-        if (filter.price.length){
-            criteria += `minprice=${filter.price[0]}&maxprice=${filter.price[1]}&`
-        }
-        if (filter.topics){
-            for(var i = 0; i < filter.topics.length; i++){
-                criteria += `&topics=${filter.topics[i]}`
-            }
-        }
-        criteria
+  var criteria = "?";
+  if (filter) {
+    if (filter.text) criteria += `text=${filter.text}&`;
+    if (filter.price.length) {
+      criteria += `minprice=${filter.price[0]}&maxprice=${filter.price[1]}&`;
     }
-    return axios
-            .get(TOPIC_TEACHER_URL + criteria)
-            .then(res => res.data)
-            .catch(e => console.log('No Topic for Teacher', e))
+    if (filter.topics) {
+      for (var i = 0; i < filter.topics.length; i++) {
+        criteria += `&topics=${filter.topics[i]}`;
+      }
+    }
+    criteria;
+  }
+  return axios
+    .get(TOPIC_TEACHER_URL + criteria)
+    .then(res => res.data)
+    .catch(e => console.log("No Topic for Teacher", e));
 }
 
-
 function saveTeacherTopic(teacherTopic) {
-    if (teacherTopic._id) return axios.put(TOPIC_TEACHER_URL, teacherTopic)
-    else return axios.post(TOPIC_TEACHER_URL, teacherTopic);  
+  if (teacherTopic._id) return axios.put(TOPIC_TEACHER_URL, teacherTopic);
+  else return axios.post(TOPIC_TEACHER_URL, teacherTopic);
 }
 
 function deleteTeacherTopic(teacherTopicId) {
-    return axios.delete(_getTeacherTopicUrl(teacherTopicId))
+  return axios.delete(_getTeacherTopicUrl(teacherTopicId));
 }
 
 function getTeacherTopicById(teacherTopicId) {
-    return axios
-    .get(_getTeacherTopicUrl(teacherTopicId))
-    .then(res => res.data)
+  return axios.get(_getTeacherTopicUrl(teacherTopicId)).then(res => res.data);
 }
 
-
 function getTopicsByTeacherId(teacherId) {
-    return axios
-    .get(_getTeacherTopicUrl(teacherId,'teacher'))
-    .then(res => res.data)
+  return axios
+    .get(_getTeacherTopicUrl(teacherId, "teacher"))
+    .then(res => res.data);
 }
 
 function getPopularTopics() {
-    return axios
-        .get('/popularTopics')
-        .then(res => res.data)
+  return axios.get("/popularTopics").then(res => res.data);
 }
 
-
-function _getTeacherTopicUrl(id,type) {
-    if (type) return `${TOPIC_TEACHER_URL}/${type}/${id}`;
-    else return `${TOPIC_TEACHER_URL}/${id}`;
+function _getTeacherTopicUrl(id, type) {
+  if (type) return `${TOPIC_TEACHER_URL}/${type}/${id}`;
+  else return `${TOPIC_TEACHER_URL}/${id}`;
 }
 
 export default {
-    getTeacherTopics,
-    saveTeacherTopic,
-    deleteTeacherTopic,
-    emptyTeacherTopic,
-    getTeacherTopicById,
-    getTopicsByTeacherId,
-    getPopularTopics
-}
+  getTeacherTopics,
+  saveTeacherTopic,
+  deleteTeacherTopic,
+  emptyTeacherTopic,
+  getTeacherTopicById,
+  getTopicsByTeacherId,
+  getPopularTopics
+};
