86fc47a5914f2ecbe52460b72dcda84b362a1762
Oz Mercer <ozmercer@gmail.com>
2018-05-07 11:48:55 +0300

    arrange navBar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -11,10 +11,10 @@
         <template v-else>
         <li></li>
         <li><router-link :to="`/profile/${loggedUser._id}`"></router-link></li>
+        <li><a href="#">Become a teacher</a></li>
         <li @click.stop><a class="profile-img" @click="toggleDropdown"><img class="profile" :src="loggedUser.img"></a></li>
         <ul class="dropdown" v-if="dropdown" @click.stop>
             <li><router-link :to="`/profile/teacher/${loggedUser._id}`">My profile</router-link></li>
-            <li><a href="#">Become a teacher</a></li>
             <li v-if="loggedUser.isTeacher"><router-link :to="`/profile/topic/${loggedUser._id}`">My classes</router-link></li>
             <li><a @click="logOut">Log out</a></li>
         </ul>
