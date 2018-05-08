6be3a452fa4acd842c5754dee18cff1ce4e7abce
Oz Mercer <ozmercer@gmail.com>
2018-05-08 19:52:04 +0300

    Prepare for merge

--- backend/services/SocketService.js
+++ backend/services/SocketService.js
@@ -1,52 +1,54 @@
-var socketIo = require("socket.io");
-var ChatService = require("./ChatService.js");
+var socketIo = require('socket.io');
+var ChatService = require('./ChatService.js');
 
 var connectedCount = 0;
 var allSockets = [];
 var rooms = [];
 function init(http) {
   const io = socketIo(http);
-  io.on("connection", socket => {
-    console.log("a user connected");
+  io.on('connection', socket => {
+    console.log('a user connected');    
     connectedCount++;
     allSockets.push(socket);
 
-    socket.on("chatRequest", data => {
-      var sortedIds = [data.senderId, data.recipientId].sort();
-      var roomId = sortedIds[0] + sortedIds[1];
+    socket.on('chatRequest', data => {
       //TODO - check if room exists
-      socket.join(roomId);
+      socket.join(data.roomId, () => {
+        io.to(data.roomId).emit('chat message',{txt: 'new user connetec to room'});    
+      });
+      
       //   io.emit('chat message', data)
     });
 
-    socket.on("disconnect", () => {
-      console.log("user disconnected");
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
       connectedCount--;
       allSockets.splice(allSockets.findIndex(s => s === socket), 1);
     });
-
-    socket.on("chat newMessage", data => {
-      var sortedIds = [data.senderId, data.recipientId].sort();
-      var roomId = sortedIds[0] + sortedIds[1];
-      console.log("NEW message: ", data);
-      ChatService.add(data);
-        io.emit('chat message', data)
-      
-    //   io.to(roomId).emit("chat message", data.txt);
+    
+    socket.on('chat newMessage', data => {
+        console.log('insided the new meesge:', data)
+        console.log('rooms:',socket.rooms);
+        // Try fix without join
+        socket.join(data.roomId, () => {
+            io.to(data.roomId).emit('chat message',data);    
+          });
+        // io.to(data.roomId).emit('chat message',data);  
+        // io.emit('chat message', data)
     });
 
-    socket.on("chat setTopic", topic => {
-      console.log("Server setting the TOPIC", topic);
-      if (socket.theTopic) socket.leave(socket.theTopic);
-      socket.join(topic);
-      socket.theTopic = topic;
+    // socket.on('chat setTopic', topic => {
+    //   console.log('Server setting the TOPIC', topic);
+    //   if (socket.theTopic) socket.leave(socket.theTopic);
+    //   socket.join(topic);
+    //   socket.theTopic = topic;
 
-      console.log("Connected Count", connectedCount, allSockets.length);
-    });
+    //   console.log('Connected Count', connectedCount, allSockets.length);
+    // });
 
-    socket.on("chat sendStatus", statusTxt => {
-      console.log("Server sending  the STATUS", statusTxt);
-      io.emit("chat setStatusTxt", statusTxt);
+    socket.on('chat sendStatus', statusTxt => {
+      console.log('Server sending  the STATUS', statusTxt);
+      io.emit('chat setStatusTxt', statusTxt);
     });
   });
 }
